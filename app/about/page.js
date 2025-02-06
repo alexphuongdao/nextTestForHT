fcf6d2d17b6a53ca2b6dae7c9672f6b7eed36a53
Phuong Alex Dao <daoduyphuong2005@gmail.com>
2025-02-05 23:59:28 -0600

    change

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+export default function AboutPage(){
+    return (
+    <main>
+        <h1>About me</h1>
+    </main>
+    );
+}
+
+// this is the page that help render the about 
+// nextJS system by folder system by itself is the 
+// structure of the actual route as well
